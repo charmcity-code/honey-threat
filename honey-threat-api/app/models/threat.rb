3000fa47c0f4ad2e88c87f0667202586a3ac8f7c
Mark Lawrence <lawrence.ml@gmail.com>
2019-05-18 15:12:47 -0400

    add model default values

--- honey-threat-api/app/models/threat.rb
+++ honey-threat-api/app/models/threat.rb
@@ -1,2 +1,6 @@
 class Threat < ApplicationRecord
+  attribute :count, :integer, default: nil
+  attribute :lat, :string, default: nil
+  attribute :long, :string, default: nil
+  attribute :country, :string, default: nil
 end
