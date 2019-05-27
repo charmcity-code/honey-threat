a1544fb6463c7f25b1a176e6d9a759351e05fdea
Mark Lawrence <lawrence.ml@gmail.com>
2019-05-27 00:47:39 -0400

    edit initialState

--- honey-threat-client/src/reducers/threatFormData.js
+++ honey-threat-client/src/reducers/threatFormData.js
@@ -1,9 +1,22 @@
-const initialState = { ip: "", count: 1, lat: "", long: "", country: "" };
+const initialState = {
+  ip: "",
+  count: getRndInteger(1, 1000),
+  lat: "",
+  long: "",
+  country: "Unknown \u{1F3F4}"
+};
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case "UPDATED_DATA":
-      return { ...state, ip: action.threatFormData.ip };
+      return {
+        ...state,
+        ip: action.threatFormData.ip
+      };
 
     case "RESET_FORM":
       return initialState;
