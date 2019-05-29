9285201ee358eefb736bf6ec5b1efcfa21c4fc46
Mark Lawrence <lawrence.ml@gmail.com>
2019-05-29 18:55:17 -0400

    add tooltip ip for map markers

--- honey-threat-client/src/components/ThreatMarker.js
+++ honey-threat-client/src/components/ThreatMarker.js
@@ -3,6 +3,6 @@ import { Marker } from "react-google-maps";
 
 export default class ThreatMarker extends React.Component {
   render() {
-    return <Marker position={this.props.location} />;
+    return <Marker position={this.props.location} title={this.props.ip} />;
   }
 }
