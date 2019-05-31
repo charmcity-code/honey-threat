7984c1a056e7277fb2e0474494f0560f23c34e58
Mark Lawrence <lawrence.ml@gmail.com>
2019-05-30 23:20:32 -0400

    add react-geocode to get lat/long from address

--- honey-threat-client/src/containers/ThreatForm.js
+++ honey-threat-client/src/containers/ThreatForm.js
@@ -2,13 +2,46 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { updateThreatFormData } from "../actions/threatForm";
 import { createThreat } from "../actions/threatActions";
+import Geocode from "react-geocode";
+Geocode.setApiKey(process.env.REACT_APP_GOOGLE_KEY);
 
 class ThreatForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      lat: "",
+      long: ""
+    };
+  }
+
+  getLatLong = location => {
+    Geocode.fromAddress(location).then(
+      response => {
+        this.setState({
+          lat: response.results[0].geometry.location.lat.toString(),
+          long: response.results[0].geometry.location.lng.toString()
+        });
+
+        console.log(this.state.lat, this.state.long);
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  };
+
   handleOnChange = event => {
-    const ip = event.target.value;
+    const { name, value } = event.target;
+
     const currentThreatFormData = Object.assign({}, this.props.threatFormData, {
-      ip
+      [name]: value,
+      lat: this.state.lat,
+      long: this.state.long
     });
+    console.log("data: ", currentThreatFormData);
+
+    this.getLatLong(this.props.threatFormData.address);
     this.props.updateThreatFormData(currentThreatFormData);
   };
 
@@ -18,17 +51,25 @@ class ThreatForm extends Component {
   };
 
   render() {
-    console.log(this.props);
     return (
       <div>
         Add New IP Threat
         <form onSubmit={this.handleOnSubmit}>
           <label>IP Address </label>
           <input
+            name='ip'
             type='text'
             onChange={this.handleOnChange}
             value={this.props.threatFormData.ip}
           />
+          <label>Location </label>
+          <input
+            name='address'
+            type='text'
+            onChange={this.handleOnChange}
+            value={this.props.threatFormData.address}
+          />
+
           <button type='submit'>Submit</button>
         </form>
       </div>
