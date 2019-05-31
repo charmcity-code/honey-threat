0df16cc00fb97c0245a392195c2ebf54b0932e04
Mark Lawrence <lawrence.ml@gmail.com>
2019-05-30 23:42:12 -0400

    edit seed data

--- honey-threat-api/db/seeds.rb
+++ honey-threat-api/db/seeds.rb
@@ -9,13 +9,13 @@
 # https://intercept.sh/threatlists/
 
 Threat.create([
-  {ip: "159.65.103.189", count: 360, lat: "37.3501", long: "-121.9850", address: "United States \u{1F1FA}\u{1F1F8}"},
-  {ip: "112.170.78.118", count: 38, lat: "37.4100", long: "127.2570", address: "South Korea \u{1F1F0}\u{1F1F7}"},
-  {ip: "134.19.187.78", count: 187, lat: "51.8656", long: "4.6629", address: "Netherlands \u{1F1F3}\u{1F1F1}"},
-  {ip: "5.188.86.167", count: 647, lat: "51.9000", long: "-8.9500", address: "Ireland \u{1F1EE}\u{1F1EA}"},
-  {ip: "103.76.252.6", count: 185, lat: "22.7167", long: "75.8333", address: "India \u{1F1EE}\u{1F1F3}"},
-  {ip: "88.214.26.90", count: 998, lat: "51.2993", long: "9.4910", address: "Germany \u{1F1E9}\u{1F1EA}"},
-  {ip: "185.220.221.221", count: 377, lat: "50.0333", long: "14.4833", address: "Czech Republic \u{1F1E8}\u{1F1FF}"},
-  {ip: "106.13.60.71", count: 498, lat: "39.9289", long: "116.3880", address: "China \u{1F1E8}\u{1F1F3}"},
-  {ip: "181.110.240.194", count: 126, lat: "-33.1416", long: "-59.3097", address: "Argentina \u{1F1E6}\u{1F1F7}"},
+  {ip: "159.65.103.189", count: 360, lat: "36.1699", long: "-115.1398", address: "Las Vegas"},
+  {ip: "112.170.78.118", count: 38, lat: "39.9526", long: "-75.1652", address: "Philadelphia"},
+  {ip: "134.19.187.78", count: 187, lat: "32.7767", long: "-96.7970", address: "Dallas"},
+  {ip: "5.188.86.167", count: 647, lat: "36.1627", long: "-86.7816", address: "Nashville"},
+  {ip: "103.76.252.6", count: 185, lat: "33.4484", long: "-112.0740", address: "Phoenix"},
+  {ip: "88.214.26.90", count: 998, lat: "47.6062", long: "-122.3321", address: "Seattle"},
+  {ip: "185.220.221.221", count: 377, lat: "40.7128", long: "-74.0060", address: "New York"},
+  {ip: "106.13.60.71", count: 498, lat: "33.6891", long: "-78.8867", address: "Myrtle Beach"},
+  {ip: "181.110.240.194", count: 126, lat: "34.0686", long: "-117.9390", address: "West Covina"},
 ])
