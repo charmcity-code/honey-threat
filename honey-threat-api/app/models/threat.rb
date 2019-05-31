6cc260b3641c116bbe30d41df76753cb59227f39
Mark Lawrence <lawrence.ml@gmail.com>
2019-05-30 23:41:56 -0400

    remove default count

--- honey-threat-api/app/models/threat.rb
+++ honey-threat-api/app/models/threat.rb
@@ -1,5 +1,4 @@
 class Threat < ApplicationRecord
-  attribute :count, :integer, default: nil
   attribute :lat, :string, default: nil
   attribute :long, :string, default: nil
   attribute :address, :string, default: nil
