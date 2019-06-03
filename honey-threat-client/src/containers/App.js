c15789116ba3a400a121fd70a3f882fbc42d14e8
Mark Lawrence <lawrence.ml@gmail.com>
2019-06-02 20:22:51 -0400

    install react-router and create routes

--- honey-threat-client/src/containers/App.js
+++ honey-threat-client/src/containers/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Threats from "./Threats";
+import NavBar from "../components/NavBar";
+import Home from "../components/Home";
+import ThreatForm from "../components/ThreatForm";
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <Threats />
-      </div>
+      <Router>
+        <NavBar />
+        <div className='App'>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/threats' component={Threats} />
+          <Route exact path='/threats/new' component={ThreatForm} />
+        </div>
+      </Router>
     );
   }
 }
