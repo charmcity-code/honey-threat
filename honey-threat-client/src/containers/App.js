cdc43a890b56f37b1ba98c793aa2761fdbcd6ae4
Mark Lawrence <lawrence.ml@gmail.com>
2019-06-03 01:22:31 -0400

    add material design bootstrap nav

--- honey-threat-client/src/containers/App.js
+++ honey-threat-client/src/containers/App.js
@@ -2,15 +2,37 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Threats from "./Threats";
-import NavBar from "../components/NavBar";
 import Home from "../components/Home";
 import ThreatForm from "../components/ThreatForm";
+import { MDBNav, MDBNavItem, MDBNavLink } from "mdbreact";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <NavBar />
+        <MDBNav>
+          <MDBNavItem>
+            <MDBNavLink disabled to='#!'>
+              Nav
+            </MDBNavLink>
+          </MDBNavItem>
+          <MDBNavItem>
+            <MDBNavLink active to='/' component={Home}>
+              Home
+            </MDBNavLink>
+          </MDBNavItem>
+          <MDBNavItem>
+            <MDBNavLink to='/threats' component={Threats}>
+              Index
+            </MDBNavLink>
+          </MDBNavItem>
+          <MDBNavItem>
+            <MDBNavLink to='/threats/new' component={ThreatForm}>
+              Create
+            </MDBNavLink>
+          </MDBNavItem>
+        </MDBNav>
+
         <div className='App'>
           <Route exact path='/' component={Home} />
           <Route exact path='/threats' component={Threats} />
