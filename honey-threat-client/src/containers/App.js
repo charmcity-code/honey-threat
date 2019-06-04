af420c0c6f4d7729bb269910509cf90479483d29
Mark Lawrence <lawrence.ml@gmail.com>
2019-06-03 20:49:28 -0400

    style nav component

--- honey-threat-client/src/containers/App.js
+++ honey-threat-client/src/containers/App.js
@@ -4,41 +4,52 @@ import "./App.css";
 import Threats from "./Threats";
 import Home from "../components/Home";
 import ThreatForm from "../components/ThreatForm";
+import ThreatIndex from "../components/ThreatIndex";
 import { MDBNav, MDBNavItem, MDBNavLink } from "mdbreact";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <MDBNav>
-          <MDBNavItem>
-            <MDBNavLink disabled to='#!'>
-              Nav
-            </MDBNavLink>
-          </MDBNavItem>
-          <MDBNavItem>
-            <MDBNavLink active to='/' component={Home}>
-              Home
-            </MDBNavLink>
-          </MDBNavItem>
-          <MDBNavItem>
-            <MDBNavLink to='/threats' component={Threats}>
-              Index
-            </MDBNavLink>
-          </MDBNavItem>
-          <MDBNavItem>
-            <MDBNavLink to='/threats/new' component={ThreatForm}>
-              Create
-            </MDBNavLink>
-          </MDBNavItem>
-        </MDBNav>
+      <div className='wrapper'>
+        <Router>
+          <MDBNav>
+            <MDBNavItem>
+              <MDBNavLink
+                style={{ color: "#0d47a1", "font-weight": "bold" }}
+                active
+                to='/'
+                component={Home}
+              >
+                Home
+              </MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink
+                style={{ color: "#0d47a1", "font-weight": "bold" }}
+                to='/threats'
+                component={Threats}
+              >
+                Current Threats
+              </MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink
+                style={{ color: "#0d47a1", "font-weight": "bold" }}
+                to='/threats/new'
+                component={ThreatForm}
+              >
+                Create New Threat
+              </MDBNavLink>
+            </MDBNavItem>
+          </MDBNav>
 
-        <div className='App'>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/threats' component={Threats} />
-          <Route exact path='/threats/new' component={ThreatForm} />
-        </div>
-      </Router>
+          <div className='App'>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/threats' component={ThreatIndex} />
+            <Route exact path='/threats/new' component={ThreatForm} />
+          </div>
+        </Router>
+      </div>
     );
   }
 }
