8d75e5315293705b4a23e4d707ab29ecbb3d5938
Mark Lawrence <lawrence.ml@gmail.com>
2019-06-03 20:47:41 -0400

    add jumbotron to home component

--- honey-threat-client/src/components/Home.js
+++ honey-threat-client/src/components/Home.js
@@ -1,18 +1,36 @@
 import React from "react";
+import { MDBJumbotron, MDBContainer, MDBRow, MDBCol } from "mdbreact";
 
-function Home() {
+const JumbotronPage = () => {
   return (
-    <div>
-      <h2>Honeypot Threatlist</h2>
-      <p>
-        These high-confidence IP addresses are gathered from attackers
-        attempting to log into a private honeypot network hosted across the
-        world. You can use this list of known attackers to hunt for comprised
-        accounts on your own systems, to populate firewall lists, or for general
-        security research purposes.
-      </p>
-    </div>
+    <MDBContainer className='mt-5 text-center'>
+      <MDBRow>
+        <MDBCol>
+          <MDBJumbotron style={{ background: "#e0e0e0" }}>
+            <h2 className='h1 display-3'>Community IP Threatlist</h2>
+            <p className='lead'>
+              These high-confidence IP addresses are gathered from attackers
+              attempting to log into a private honeypot network hosted across
+              the world. You can use this list of known attackers to hunt for
+              comprised accounts on your own systems, to populate firewall
+              lists, or for general security research purposes.
+            </p>
+            <hr className='my-2' />
+            <p style={{ color: "#3f729b" }}>
+              2019 -{" "}
+              <a
+                style={{ color: "#3f729b" }}
+                href='http://github.com/lulabromark'
+                target='_blank'
+              >
+                https://github.com/lulabromark
+              </a>
+            </p>
+          </MDBJumbotron>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
-}
+};
 
-export default Home;
+export default JumbotronPage;
