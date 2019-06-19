c992ce10b44e57eb7c74ca86bf1ff1373150a245
Mark Lawrence <lawrence.ml@gmail.com>
2019-06-19 11:37:25 -0400

    edit comment view to properly display comments

--- honey-threat-client/src/components/CommentView.js
+++ honey-threat-client/src/components/CommentView.js
@@ -4,9 +4,7 @@ class CommentView extends Component {
   render() {
     return (
       <div>
-        {this.props.comment}
-        {this.props.likeNumber}
-        <button onSubmit={this.props.handleVote}>test</button>
+        {this.props.comment} {this.props.button} {this.props.votes}
       </div>
     );
   }
